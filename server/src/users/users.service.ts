172a88c3dfde493add75822b05525bfd685d7ee6
f1aring <arman.rahman@g.bracu.ac.bd>
2025-12-26 16:24:20 +0600

    fix: user service updated

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepo.find();
+  }
+
+  findById(id: number): Promise<User | null> {
+    return this.userRepo.findOne({ where: { id } });
+  }
+}
+
