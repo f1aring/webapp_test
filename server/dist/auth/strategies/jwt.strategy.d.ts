95cca6a5463ea289e874bf75381374b51bc0777f
f1aring <arman.rahman@g.bracu.ac.bd>
2025-12-26 18:58:43 +0600

    fix: some changes are made in rregarding the packages

--- server/dist/auth/strategies/jwt.strategy.d.ts
+++ server/dist/auth/strategies/jwt.strategy.d.ts
@@ -1,5 +1,6 @@
+import { Strategy } from 'passport-jwt';
 import { AuthService } from '../auth.service';
-declare const JwtStrategy_base: new (...args: any[]) => any;
+declare const JwtStrategy_base: new (...args: any[]) => Strategy;
 export declare class JwtStrategy extends JwtStrategy_base {
     private readonly authService;
     constructor(authService: AuthService);
